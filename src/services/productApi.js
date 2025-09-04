733ab1048124d2e809432031ba98a7ab9c89d4b0
Abhey Khurana <abheykhurana73@gmail.com>
2025-09-04 13:54:31 +0530

    Api Change

--- src/services/productApi.js
+++ src/services/productApi.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import { API_BASE } from "../config.js";
 
 export const getProducts = async () => {
-  const res = await axios.get(`${API_BASE}/products`);
+  const res = await axios.get(`${API_BASE}/api/products`);
   return res.data;
 };
 
 export const getProduct = async (id) => {
-  const res = await axios.get(`${API_BASE}/products/${id}`);
+  const res = await axios.get(`${API_BASE}/api/products/${id}`);
   return res.data;
 };
